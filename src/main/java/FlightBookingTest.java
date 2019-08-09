901855da3e52ff21325e5442c345fcaadc164696
Pratikshasoni <53863428+Pratikshasoni@users.noreply.github.com>
2019-08-09 20:16:41 +0530

    Update FlightBookingTest.java

--- src/main/java/FlightBookingTest.java
+++ src/main/java/FlightBookingTest.java
@@ -4,40 +4,44 @@ import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class FlightBookingTest {
 
     WebDriver driver = new ChromeDriver();
-
 
     @Test
     public void testThatResultsAppearForAOneWayJourney() {
 
         setDriverPath();
         driver.get("https://www.cleartrip.com/");
-        waitFor(2000);
+       
+        
         driver.findElement(By.id("OneWay")).click();
-
         driver.findElement(By.id("FromTag")).clear();
         driver.findElement(By.id("FromTag")).sendKeys("Bangalore");
 
         //wait for the auto complete options to appear for the origin
-
-        waitFor(2000);
         List<WebElement> originOptions = driver.findElement(By.id("ui-id-1")).findElements(By.tagName("li"));
-        originOptions.get(0).click();
-
-        driver.findElement(By.id("toTag")).clear();
-        driver.findElement(By.id("toTag")).sendKeys("Delhi");
-
-        //wait for the auto complete options to appear for the destination
-
-        waitFor(2000);
+        
+        for(WebElement element : originOptions) {
+        	if (element.getText().contains("Bangalore")) {
+        		element.click();
+        		break;
+        	}
+        }
+       
+        driver.findElement(By.xpath("//*[@id=\"ToTag\"]")).clear();
+       
+        driver.findElement(By.id("//*[@id=\'ToTag\'][2]")).sendKeys("New Delhi");
+        
         //select the first item from the destination auto complete list
         List<WebElement> destinationOptions = driver.findElement(By.id("ui-id-2")).findElements(By.tagName("li"));
         destinationOptions.get(0).click();
@@ -47,7 +51,7 @@ public class FlightBookingTest {
         //all fields filled in. Now click on search
         driver.findElement(By.id("SearchBtn")).click();
 
-        waitFor(5000);
+       // waitFor(5000);
         //verify that result appears for the provided journey search
         Assert.assertTrue(isElementPresent(By.className("searchSummary")));
 
@@ -55,9 +59,10 @@ public class FlightBookingTest {
         driver.quit();
 
     }
+    
+   
 
-
-    private void waitFor(int durationInMilliSeconds) {
+   private void waitFor(int durationInMilliSeconds) {
         try {
             Thread.sleep(durationInMilliSeconds);
         } catch (InterruptedException e) {
@@ -87,3 +92,4 @@ public class FlightBookingTest {
         }
     }
 }
+
