07728bf1dd9b303d9eba7387937a7bc975e7108d
Pratikshasoni <53863428+Pratikshasoni@users.noreply.github.com>
2019-08-09 20:09:50 +0530

    Update HotelBookingTest.java

--- src/main/java/HotelBookingTest.java
+++ src/main/java/HotelBookingTest.java
@@ -1,17 +1,27 @@
+package CodingRound;
+
 import com.sun.javafx.PlatformUtil;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
-public class HotelBookingTest {
+public class HotelBookingTest{
 
     WebDriver driver = new ChromeDriver();
 
     @FindBy(linkText = "Hotels")
     private WebElement hotelLink;
+  
 
     @FindBy(id = "Tags")
     private WebElement localityTextBox;
@@ -27,9 +37,14 @@ public class HotelBookingTest {
         setDriverPath();
 
         driver.get("https://www.cleartrip.com/");
-        hotelLink.click();
+       
+        By hotelLink =By.linkText("Hotels");
+        getElement(driver,hotelLink).click();
 
-        localityTextBox.sendKeys("Indiranagar, Bangalore");
+       By localityTextBox =By.id("Tags");
+       getElement(driver,localityTextBox).sendKeys("Indiranagar, Bangalore");
+      List<WebElement> location = driver.findElement(By.id("ui-id-1")).findElements(By.tagName("li"));
+    
 
         new Select(travellerSelection).selectByVisibleText("1 room, 2 adults");
         searchButton.click();
@@ -37,16 +52,23 @@ public class HotelBookingTest {
         driver.quit();
 
     }
+    
+    public static WebElement getElement(WebDriver driver, By locator) {
+    	WebDriverWait wait  = new WebDriverWait(driver,15);
+    	wait.until(ExpectedConditions.presenceOfElementLocated(locator));
+    	WebElement element =driver.findElement(locator);
+    	return element;
+    }
 
     private void setDriverPath() {
-        if (PlatformUtil.isMac()) {
-            System.setProperty("webdriver.chrome.driver", "chromedriver");
-        }
-        if (PlatformUtil.isWindows()) {
-            System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-        }
-        if (PlatformUtil.isLinux()) {
-            System.setProperty("webdriver.chrome.driver", "chromedriver_linux");
+    	 if (PlatformUtil.isMac()) {
+             System.setProperty("webdriver.chrome.driver", "chromedriver");
+         }
+         else if (PlatformUtil.isWindows()) {
+             System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+         }
+         else if (PlatformUtil.isLinux()) {
+             System.setProperty("webdriver.chrome.driver", "chromedriver_linux");
         }
     }
 
