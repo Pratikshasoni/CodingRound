067fa1b78758a4940864ba8063df7296fc3f1024
Pratikshasoni <53863428+Pratikshasoni@users.noreply.github.com>
2019-08-09 14:40:54 +0530

    Update SignInTest.java

--- src/main/java/SignInTest.java
+++ src/main/java/SignInTest.java
@@ -1,6 +1,12 @@
+package CodingRound;
+
 import com.sun.javafx.PlatformUtil;
+
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -15,37 +21,33 @@ public class SignInTest {
         setDriverPath();
 
         driver.get("https://www.cleartrip.com/");
-        waitFor(2000);
+        driver.manage().timeouts().implicitlyWait(20,TimeUnit.SECONDS);
+        //wait
 
         driver.findElement(By.linkText("Your trips")).click();
         driver.findElement(By.id("SignIn")).click();
-
+        driver.switchTo().frame("modal_window");
         driver.findElement(By.id("signInButton")).click();
-
+     
         String errors1 = driver.findElement(By.id("errors1")).getText();
         Assert.assertTrue(errors1.contains("There were errors in your submission"));
         driver.quit();
     }
 
-    private void waitFor(int durationInMilliSeconds) {
-        try {
-            Thread.sleep(durationInMilliSeconds);
-        } catch (InterruptedException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-        }
-    }
 
     private void setDriverPath() {
         if (PlatformUtil.isMac()) {
             System.setProperty("webdriver.chrome.driver", "chromedriver");
         }
-        if (PlatformUtil.isWindows()) {
+        else if (PlatformUtil.isWindows()) {
             System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
         }
-        if (PlatformUtil.isLinux()) {
+        else if (PlatformUtil.isLinux()) {
             System.setProperty("webdriver.chrome.driver", "chromedriver_linux");
         }
     }
 
 
 }
+
+      
